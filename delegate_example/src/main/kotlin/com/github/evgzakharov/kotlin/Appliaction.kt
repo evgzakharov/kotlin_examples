ae28f3b12fb707c3a718a64c4c6eed667777411c
Evgeny Zaharov <EZakharov2@alfabank.ru>
2017-11-17 17:51:37 +0300

    add delegated examples

---
+++ delegate_example/src/main/kotlin/com/github/evgzakharov/kotlin/Appliaction.kt
@@ -0,0 +1,5 @@
+package com.github.evgzakharov.kotlin
+
+fun main(args: Array<String>) {
+
+}
---
+++ delegate_example/src/main/kotlin/com/github/evgzakharov/kotlin/Application2.kt
@@ -0,0 +1,100 @@
+package com.github.evgzakharov.kotlin
+
+import java.util.*
+import kotlin.properties.Delegates
+import kotlin.properties.ReadOnlyProperty
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+fun main(args: Array<String>) {
+
+    val someValue: String by SomeOwnDelegate()
+
+    println(someValue)
+
+    val lazyInitValue: String by lazy {
+        println("init")
+
+        "I has been created!"
+    }
+
+    println("do you created?")
+
+    println(lazyInitValue)
+
+    var someMutableValue: String by SomeOwnDelegateMutable("someOtherValue")
+
+    println(someMutableValue)
+    println(someMutableValue)
+    println(someMutableValue)
+
+    someMutableValue = "anotherValue"
+
+    println(someMutableValue)
+    println(someMutableValue)
+    println(someMutableValue)
+
+    var state: Int by Delegates.observable(5) { property , old , new ->
+        println("state has been changes, old value: $old, new value: $new")
+    }
+
+    state = 10
+    state = 12
+
+    println(state)
+
+
+    val propertyMap = mapOf("userName" to "Vasua", "password" to "Gektor")
+
+    val userName by propertyMap
+    val password by propertyMap
+
+    println("userName: $userName; password: $password")
+
+    var ariaStark = 9
+    println(ariaStark.whoAreYou)
+
+    ariaStark = 15
+    println(ariaStark.whoAreYou)
+}
+
+data class User(val name: String, val surname: String)
+
+val Int.whoAreYou: String by ExtensionDelegate()
+
+class ExtensionDelegate : ReadOnlyProperty<Int, String> {
+    override fun getValue(thisRef: Int, property: KProperty<*>) = if (thisRef < 10) {
+        "Aria Stark!"
+    } else {
+        "Nobody.."
+    }
+}
+
+class SomeOwnDelegateMutable(srcValue: String) : ReadWriteProperty<Nothing?, String> {
+    private var realValue: String = srcValue
+
+    private fun randomCase() {
+        val random = Random().nextBoolean()
+
+        realValue = if (random) {
+            realValue.toUpperCase()
+        } else {
+            realValue.toLowerCase()
+        }
+    }
+
+    override fun getValue(thisRef: Nothing?, property: KProperty<*>): String {
+        randomCase()
+
+        return realValue
+    }
+
+    override fun setValue(thisRef: Nothing?, property: KProperty<*>, value: String) {
+        realValue = value
+    }
+
+}
+
+class SomeOwnDelegate : ReadOnlyProperty<Nothing?, String> {
+    override fun getValue(thisRef: Nothing?, property: KProperty<*>): String = property.name
+}
