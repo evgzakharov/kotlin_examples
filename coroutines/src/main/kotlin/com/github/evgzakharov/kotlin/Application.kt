9f546d0748b2ee3fd8d2a17fa64bdb050ac32157
evgzakharov <evgzakharov88@gmail.com>
2017-11-17 19:21:39 +0300

    add coroutine example

---
+++ coroutines/src/main/kotlin/com/github/evgzakharov/kotlin/Application.kt
@@ -0,0 +1,25 @@
+package com.github.evgzakharov.kotlin
+
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.runBlocking
+
+fun main(args: Array<String>) = runBlocking {
+    val job1 = async { one() }
+    val job2 = async { two() }
+
+    println("step 3")
+
+    listOf(job1, job2).forEach { it.join() }
+}
+
+suspend fun one(): Unit {
+    delay(100)
+    println("step 1")
+}
+
+
+suspend fun two(): Unit {
+    delay(200)
+    println("step 2")
+}
